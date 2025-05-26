1f6663d2d2b1ec99a74d1488648d5c63ffdf5aab
Daniel Monção Pontes <119755463+Danielmoncao05@users.noreply.github.com>
2025-05-26 14:39:06 -0300

    Update WebSocketConfig.java

--- src/main/java/com/senai/projeto_auth_ws/infrastructure/websocket/WebSocketConfig.java
+++ src/main/java/com/senai/projeto_auth_ws/infrastructure/websocket/WebSocketConfig.java
@@ -7,19 +7,19 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocketMessageBroker  //
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {  // websocket = protocolo bidirecional e usa uma conexao , // configuração websocket
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic", "/queue");
-        config.setApplicationDestinationPrefixes("/app");
+    public void configureMessageBroker(MessageBrokerRegistry config) { // 
+        config.enableSimpleBroker("/topic", "/queue"); // topico / fila
+        config.setApplicationDestinationPrefixes("/app"); // caminho principal para todos x
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
+        registry.addEndpoint("/ws") // endpoint principal 
+                .setAllowedOriginPatterns("*") // especificação 
                 .withSockJS();
     }
 }
